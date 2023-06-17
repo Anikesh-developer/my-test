2bff18a5fdf31cbc67fe8ce9ae9edcd73d74bc74
Anikesh-developer <anikesh.dsce98@gmail.com>
2023-06-17 21:11:41 +0530

    commit 3

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -3,9 +3,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
 import Clothing from '../../Pages/Clothing/Clothing';
-import Search from '../../Pages/Search/Search';
 import './header.css';
-import Category from '../Category/Category';
 
 const Header = () => {
     const[content, setContent] = useState([]);
@@ -22,8 +20,6 @@ const Header = () => {
 
   return (
     <div className='header'>
-        <Search />
-        <Category />
         <div className='content'>
             {
                 content && content?.map((c) => <Clothing key={c.id} id={c.id} title={c.title} category={c.category} price={c.price} description={c.description} image={c.image} />)
